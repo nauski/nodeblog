e4efb77e75ac52fc71289b113bad088dc2e4e9a3
nauski <nauski@nauski.com>
2016-06-21 14:59:09 +0300

    added authentication with passport

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,10 @@ var router = express.Router();
 var multer = require('multer');
 var upload = multer({dest: './uploads'});
 var User = require('../models/users');
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -16,6 +20,39 @@ router.get('/login', function(req, res, next) {
    res.render('login', {title: 'Login'}); 
 });
 
+router.post('/login',
+  passport.authenticate('local',{failureRedirect: '/users/login', failureFlash: 'Invalid username or password'}),
+  function(req, res) {
+    req.flash('success', 'You are now logged in');
+    res.redirect('/');
+  });
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(function(username, password, done){
+    User.getUserByUsername(username, function(err, user){
+    if(err) throw err;
+    if(!user){
+        return done(null, false, {message: 'Unknown user'});
+    }    
+    User.comparePassword(password, user.password, function(error, isMatch){
+        if(err) return done(err);
+        if(isMatch){
+            return done(null, user);
+    } else {
+        return done(null, false, {message: 'Invalid Password'});
+        }
+    });
+});
+}));
 router.post('/register', upload.single('profileimage'), function(req, res, next) {
     var name = req.body.name;
     var email = req.body.email;
